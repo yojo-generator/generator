fda709bea3d29d1af301cee102e4ae74fae7ba25
Морозкин Владимир <vladimir.morozkin@gazprombank.ru>
2023-03-27 18:11:37 +0300

    update

---
+++ src/main/java/ru/yojo/codegen/generator/YojoApiGenerator.java
@@ -0,0 +1,15 @@
+package ru.yojo.codegen.generator;
+
+import ru.yojo.codegen.domain.LombokProperties;
+
+/**
+ * Will add in the future
+ * Generates asyncApi from javaClasses
+ */
+@SuppressWarnings("all")
+public class YojoApiGenerator implements Generator {
+    @Override
+    public void generate(String filePath, String outputDirectory, LombokProperties lombokProperties) {
+
+    }
+}
